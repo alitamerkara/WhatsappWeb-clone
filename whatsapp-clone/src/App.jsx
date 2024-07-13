a84fe5a42f9db41bcb1011202b1b731abe795f07
alitamerkara <alitamerkara1907@gmail.com>
2024-07-13 14:30:31 +0300

    i started to create my project and i added firebase config lines.

---
+++ whatsapp-clone/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="bg-orange-100 w-screen h-screen flex items-center justify-center">
+        <div className="w-3/5 h-5/6 bg-white shadow-2xl"></div>
+      </div>
+    </>
+  );
+}
+
+export default App;
